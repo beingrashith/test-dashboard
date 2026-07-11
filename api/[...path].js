473e1a2d0bf4845337b84f3e26cbd845d36220f2
beingrashith <mmrashith18@gmail.com>
2026-07-11 16:06:58 +0530

    path update

--- api/[...path].js
+++ api/[...path].js
@@ -1,29 +1,7 @@
-export default async function handler(req, res) {
-  const { path } = req.query;
-
-  const targetUrl = `https://admin-moderator-backend-staging.up.railway.app/api/${path.join("/")}`;
-
-  try {
-    const response = await fetch(targetUrl, {
-      method: req.method,
-      headers: {
-        "Content-Type": "application/json",
-        ...(req.headers.authorization
-          ? { Authorization: req.headers.authorization }
-          : {}),
-      },
-      body:
-        req.method === "GET" || req.method === "HEAD"
-          ? undefined
-          : JSON.stringify(req.body),
-    });
-
-    const data = await response.text();
-
-    res.status(response.status).send(data);
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+    path: req.query.path,
+  });
 }
